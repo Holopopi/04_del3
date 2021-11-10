c5bfe716e5484c61fc7cc3b0f6d7bfc1c7d162de
marcusjacobsen <S215799@dtu.dk>
2021-11-10 21:51:48 +0100

    Game klassen ændret i Marcus branch. Den laver en plade og spørger efter antal spillere. De bliver så oprettet på pladen.

--- src/main/java/Spiller.java
+++ src/main/java/Spiller.java
@@ -1,9 +1,12 @@
 //Denne klasse bruges til at lave spilleren, samt opretter en saldo, navn og gør det muligt at skifte tur
 
+import gui_fields.GUI_Player;
+
 public class Spiller {
     Konto konto;
     String navn;
     Boolean tur;
+    GUI_Player player;
 
     public Spiller(int saldo,String navn){
         if (saldo<0){
@@ -13,10 +16,16 @@ public class Spiller {
             this.konto = new Konto(saldo);
         }
         this.navn=navn;
+        this.player = new GUI_Player(navn,saldo);
     }
     public void saldoOpdatering(int saldoOpdatering){
         this.konto.addSaldo(saldoOpdatering);
     }
+
+    public GUI_Player getPlayer(){
+        return this.player;
+    }
+
 
     public void setTur(Boolean tur) {
         this.tur = tur;
