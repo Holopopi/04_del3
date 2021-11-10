7e985cc3f476bb6276770af66ac079044276226f
marcusjacobsen <S215799@dtu.dk>
2021-11-10 19:45:37 +0100

    Module ændret. Color added

--- src/main/java/MarcusTester.java
+++ src/main/java/MarcusTester.java
@@ -2,10 +2,7 @@ import gui_main.GUI;
 
 public class MarcusTester {
     public static void main(String[] args) {
-        GameBoard gameboard = new GameBoard();
 
-
-        GUI gui = new GUI(gameboard.getGuiFields());
 
     }
 }
