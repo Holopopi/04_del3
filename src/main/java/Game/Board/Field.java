8e9829eeceb34c8b56607a0805cddb158db538f4
marcusjacobsen <S215799@dtu.dk>
2021-11-11 12:49:08 +0100

    Kommentar til Field klassen tilføjet

--- src/main/java/Game/Board/Field.java
+++ src/main/java/Game/Board/Field.java
@@ -11,7 +11,9 @@ public abstract class Field {
     private String subText;
     protected GUI_Field guiField;
 
-
+    /**
+     * Constructor for Field
+     */
     public Field(String title, String description, String subText, Color backgroundColor, Color foregroundColor){
         this.guiField=createGUIField();
         this.setTitle(title);
@@ -21,40 +23,76 @@ public abstract class Field {
         this.setForegroundColor(foregroundColor);
     }
 
+    /**
+     * Setter for feltets navn
+     */
     protected Field setTitle(String title){
         this.guiField.setTitle(title);
         return this;
     }
+
+    /**
+     * Setter for feltets beskrivelse
+     */
     protected Field setDescription(String description){
         this.guiField.setDescription(description);
         return this;
     }
+
+    /**
+     * Setter for feltets sub tekst
+     */
     protected Field setSubText(String subText){
         this.guiField.setSubText(subText);
         return this;
     }
+
+    /**
+     * Setter for baggrundsfarven
+     */
     public Field setBackgroundColor(Color backgroundColor){
         this.guiField.setBackGroundColor(backgroundColor);
-
         return this;
     }
+
+    /**
+     * Setter for feltets baggrundsfarve
+     */
     public Field setForegroundColor(Color foregroundColor){
         this.guiField.setForeGroundColor(foregroundColor);
         return this;
     }
 
+    /**
+     * Getter for feltets titel
+     */
     protected String getTitle(){
         return this.guiField.getTitle();
     }
+
+    /**
+     * Getter for feltets beskrivelse
+     */
     protected String getDescription(){
         return this.guiField.getDescription();
     }
+
+    /**
+     * Getter for feltets sub tekst
+     */
     protected String getSubText(){
         return this.guiField.getSubText();
     }
 
+
+    /**
+     * Abstrakt metode for oprettelse af et GUI felt
+     */
     abstract protected GUI_Field createGUIField();
 
+    /**
+     * Abstrakt getter for feltets GUI felt
+     */
     abstract public GUI_Field getGuiField();
 
 }
